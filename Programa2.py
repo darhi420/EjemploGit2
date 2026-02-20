fd9a588ee36810670401624c1fa210cc56d9c314
alejoo <daolarte92@ucatolica.edu.co>
2026-02-20 12:16:19 -0500

    Comit actualizando los archivos modificados

--- Programa2.py
+++ Programa2.py
@@ -2,4 +2,4 @@ base = int(input("Ingrese la base del triangulo"))
 altura = int(input("Ingrese la altura del triangulo"))
 
 area = (base * altura) / 2 
-print("El area del triangulo es de" ,area)
+print("El area del triangulo es de;" ,area)
